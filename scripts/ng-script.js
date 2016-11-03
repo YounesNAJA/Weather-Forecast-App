fd91fa4389319eeb314da9ca54b86fb586f39181
Younes NAJA <naja.younes@gmail.com>
2016-11-03 03:37:34 +0000

    OpenWeatherMap API integration
    OpenWeatherMap API integration

--- scripts/ng-script.js
+++ scripts/ng-script.js
@@ -6,25 +6,52 @@ var weather = angular.module("weather", ["ngRoute", "ngResource"]);
 /*
  * ROUTE
  */
-weather.config(function($routeProvider){
+weather.config(function ($routeProvider) {
     $routeProvider
-    .when("/", {
-        templateUrl: 'pages/main.html',
-        controller: 'homeController'
-    })
-    .when("/forecast", {
-        templateUrl: 'pages/forecast.html',
-        controller: 'forecastController'
-    });
+        .when("/", {
+            templateUrl: 'pages/home.html',
+            controller: 'homeController'
+        })
+        .when("/forecast", {
+            templateUrl: 'pages/forecast.html',
+            controller: 'forecastController'
+        });
 });
+
+/*
+ * SERVICE
+ */
+weather.service("dataShare", function () {
+    this.city = "Marrakech";
+})
 
 /* 
  * CONTROLLERS
  */
-weather.controller("homeController", ['$scope', function($scope){
-    $scope.city = city;
+weather.controller("homeController", ['$scope', 'dataShare', function ($scope, dataShare) {
+    $scope.city = dataShare.city;
+
+    $scope.$watch('city', function () {
+        dataShare.city = $scope.city;
+    });
 }]);
 
-weather.controller("forecastController", ["$scope", function($scope){
-    
+
+weather.controller("forecastController", ["$scope", "$resource", "dataShare", function ($scope, $resource, dataShare) {
+    $scope.city = dataShare.city;
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast", {
+        callback: "JSON_CALLBACK"
+    }, {
+        get: {
+            method: "JSONP"
+        }
+    });
+
+    $scope.weatherData = $scope.weatherAPI.get({
+        q: $scope.city,
+        cnt: 2,
+        appid: '7661df8e5737e8855c66e396f40b3318'
+    });
+
+    console.log($scope.weatherData);
 }]);
